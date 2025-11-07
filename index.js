ed1d7e4163174360b3e3116a2e4f8a7433b22cf9
AlekBalc <balc.aleksas@gmail.com>
2025-11-07 17:30:16 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.all("/", async (req, res) => {
+  const duration = 1000 * 60 * 6; // 6 minutes
+  await new Promise((resolve) => setTimeout(resolve, duration));
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
