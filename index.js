954b808c50d90c24b8d79c1f1345959368d33211
AlekBalc <balc.aleksas@gmail.com>
2025-11-26 18:55:04 +0200

    improvement

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import { airportData } from "./airport.js";
 const app = express();
 const port = 3000;
 
-app.all("/airport/{*splat}", async (req, res) => {
+app.all("/airport{*splat}", async (req, res) => {
   console.log("Airport endpoint hit");
   res.json(airportData);
 });
 
-app.all("/timeout/{*splat}", async (req, res) => {
+app.all("/timeout{*splat}", async (req, res) => {
   console.log("Timeout endpoint hit");
   const duration = 1000 * 60 * 6; // 6 minutes
   await new Promise((resolve) => setTimeout(resolve, duration));
