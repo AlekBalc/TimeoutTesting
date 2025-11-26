4a02aa30ee36642e0f6bb26c8e206c98bdf3d510
AlekBalc <balc.aleksas@gmail.com>
2025-11-26 18:53:28 +0200

    rename timeout endpoint

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.all("/airport/{*splat}", async (req, res) => {
   res.json(airportData);
 });
 
-app.all("/{*splat}", async (req, res) => {
-  console.log("Timouet endpoint hit");
+app.all("/timeout/{*splat}", async (req, res) => {
+  console.log("Timeout endpoint hit");
   const duration = 1000 * 60 * 6; // 6 minutes
   await new Promise((resolve) => setTimeout(resolve, duration));
   res.send("Hello World!");
